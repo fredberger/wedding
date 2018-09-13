554e25692ea81c2c17ca6b7b32aca0d8cbb8d540
Frederico Berger <fredericoberger@gmail.com>
2018-09-13 20:27:31 -0300

    Version 2.1

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   match "/guests/:id" => 'guests#update', via: :put
   match "/guests" => 'guests#index', via: :get
+  match "/chabarsp" => 'home#bar', via: :get
   match "/messages" => 'messages#create', via: :post
 end
